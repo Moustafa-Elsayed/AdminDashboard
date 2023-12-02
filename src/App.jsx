94c34d136da28f0817c4bcdda51a27c9377e1c1f
Mostafa Elsayed <moelsayed949@gmail.com>
2023-12-03 01:18:28 +0200

    create appbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Appbar from "./componenets/appbar/Appbar";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-  hello
+      <Appbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
