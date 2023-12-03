1a12b1e89f4e575e6ffe776894cba974ed06850f
Mostafa Elsayed <moelsayed949@gmail.com>
2023-12-03 03:00:52 +0200

    styling navbar

--- src/componenets/navbar/NavBar.jsx
+++ src/componenets/navbar/NavBar.jsx
@@ -1,7 +1,9 @@
 import Toolbar from "@mui/material/Toolbar";
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconButton, Typography, styled } from "@mui/material";
+import { IconButton, InputBase, Stack, Typography, alpha, styled } from "@mui/material";
 import MuiAppBar from "@mui/material/AppBar";
+import SearchIcon from "@mui/icons-material/Search";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const drawerWidth = 240;
 
@@ -23,13 +25,53 @@ const AppBar = styled(MuiAppBar, {
     }),
   }),
 }));
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
  // eslint-disable-next-line react/prop-types
  const NavBar = ({ handleDrawerOpen, open }) => {
 
    return (
-     <AppBar position="fixed" 
-// @ts-ignore
-     open={open}>
+     <AppBar
+       position="fixed"
+       // @ts-ignore
+       open={open}
+     >
        <Toolbar>
          <IconButton
            color="inherit"
@@ -46,6 +88,28 @@ const AppBar = styled(MuiAppBar, {
          <Typography variant="h6" noWrap component="div">
            Admin dashborad
          </Typography>
+         <Search>
+           <SearchIconWrapper>
+             <SearchIcon />
+           </SearchIconWrapper>
+           <StyledInputBase
+             placeholder="Search…"
+             inputProps={{ "aria-label": "search" }}
+           />
+         </Search>
+         <Stack flexGrow={1} />
+         <IconButton size="small" color="inherit">
+           <DeleteIcon />
+         </IconButton>
+         <IconButton size="small" color="inherit">
+           <DeleteIcon />
+         </IconButton>
+         <IconButton size="small" color="inherit">
+           <DeleteIcon />
+         </IconButton>
+         <IconButton size="small" color="inherit">
+           <DeleteIcon />
+         </IconButton>
        </Toolbar>
      </AppBar>
    );
