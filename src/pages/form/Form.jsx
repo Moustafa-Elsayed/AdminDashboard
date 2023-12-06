d30a76084c03dcbe6fac191b971c560f3a182018
Mostafa Elsayed <moelsayed949@gmail.com>
2023-12-06 22:58:52 +0200

    update form selection

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -1,8 +1,22 @@
-import { Box, FormControl, InputLabel, MenuItem, Stack, Typography } from "@mui/material";
+import { Box, MenuItem, Stack, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
-import Select from "@mui/material/Select";
 import Button from "@mui/material/Button";
 
+const currencies = [
+  {
+    value: "User",
+    label: "User",
+  },
+  {
+    value: "Manager",
+    label: "Manager",
+  },
+  {
+    value: "Admin",
+    label: "Admin",
+  },
+ 
+];
 const Form = () => {
   return (
     <>
@@ -41,20 +55,23 @@ const Form = () => {
         <TextField id="filled-basic" label="Contact Number" variant="filled" />
         <TextField id="filled-basic" label="Address 1" variant="filled" />
         <TextField id="filled-basic" label="Adress 2" variant="filled" />
-        <FormControl variant="filled" sx={{ minWidth: 120 }}>
-          <InputLabel id="demo-simple-select-filled-label">Role</InputLabel>
-          <Select
-            labelId="demo-simple-select-filled-label"
-            id="demo-simple-select-filled"
-          >
-            <MenuItem value=""></MenuItem>
-            <MenuItem value={10}>User</MenuItem>
-            <MenuItem value={20}>Managr</MenuItem>
-            <MenuItem value={30}>Admin</MenuItem>
-          </Select>
-        </FormControl>
+        <TextField
+          id="outlined-select-currency"
+          variant="filled"
+          select
+          label="Role"
+          defaultValue="User"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
         <Stack spacing={2} direction="row" justifyContent="flex-end">
-          <Button sx={{fontSize:"18px"}} variant="contained">Create New User</Button>
+          <Button sx={{ fontSize: "18px" }} variant="contained">
+            Create New User
+          </Button>
         </Stack>
       </Box>
     </>
